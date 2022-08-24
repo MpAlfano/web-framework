5662b8c78c49bd1243479a59c6ddd9106fd81ebe
Mark Alfano <mpalfano4@gmail.com>
2022-08-23 22:52:20 -0400

    creating new classes to handle methods for better reusability

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ id: 1 });
 
-user.set({ name: "new", age: 400 });
+user.events.on("change", () => {
+  console.log("change");
+});
 
-user.save();
+user.events.trigger("change");
