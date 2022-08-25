b6061674a014cc9c9c1b89fab6f54b6cd4603577
Mark Alfano <mpalfano4@gmail.com>
2022-08-25 15:16:34 -0400

    rebuilt to use composition and created composed object through static class method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = User.buildUser({ id: 2 });
 
-user.events.on("change", () => {
-  console.log("change");
+user.on("change", () => {
+  console.log(user);
 });
 
-user.events.trigger("change");
+user.fetch();
