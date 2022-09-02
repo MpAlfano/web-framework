991b4d117dd2c99ad0fb906575160efd3d71c21c
Mark Alfano <mpalfano4@gmail.com>
2022-09-01 21:26:14 -0400

    implementing collection display

--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -1,6 +1,6 @@
 import { Collection } from "../models/Collection";
 
-export abstract class CollectionViews<T, K> {
+export abstract class CollectionView<T, K> {
   constructor(public parent: Element, public collection: Collection<T, K>) {}
 
   abstract renderItem(model: T, itemParent: Element): void;
